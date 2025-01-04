53445811f5644ec66ec2e1c9280c6209224d2c0a
Naseebullah-Wali <70603493+Naseebullah-Wali@users.noreply.github.com>
2025-01-04 19:09:57 +0100

    project is on, bootstrap needed to be added

---
+++ project-management-app/src/views/ProjectView.vue
@@ -0,0 +1,17 @@
+<template>
+    <div id="app">
+        <h1>Here are list of projects</h1>
+        <ProjectList />
+    </div>
+ 
+</template>
+
+<script>
+import ProjectList from '../components/ProjectList.vue';
+
+export default {
+  components: {
+    ProjectList
+  }
+};
+</script>
