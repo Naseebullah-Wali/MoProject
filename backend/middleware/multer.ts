225e6026ef40540b8899f41514eb0e6b7f2a9b28
Naseebullah-Wali <70603493+Naseebullah-Wali@users.noreply.github.com>
2025-02-24 02:58:58 +0100

    multer

--- backend/middleware/multer.ts
+++ backend/middleware/multer.ts
@@ -1,10 +1,13 @@
+import { RequestHandler } from 'express';
 import multer from 'multer';
 
-// Configure Multer for file uploads
 const storage = multer.memoryStorage();
 const upload = multer({
   storage: storage,
   limits: { fileSize: 5 * 1024 * 1024 } // Limit file size to 5MB
 });
 
-export default upload;
+// Explicitly cast the type if necessary
+const yourHandler = upload.single('file') as RequestHandler;
+
+export default yourHandler;
